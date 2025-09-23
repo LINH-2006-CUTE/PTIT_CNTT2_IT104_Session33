b640ee9ad289eaa1ac6b586105051891b6aed742
Lê Nhật Linh <lenhatlinh19042006@nh19042006gmail.42006gmailcom>
2025-09-23 07:28:43 +0700

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import ShoppingForm from "./components/ShoppingForm";
 import Cart from "./components/ProductCart";
-// import Notification from "./components/Notification";
 import "./App.css";
 
 function App() {
@@ -11,9 +10,17 @@ function App() {
         <ShoppingForm/>
         <Cart />
       </div>
-      {/* <Notification /> */}
     </div>
   );
 }
 
 export default App;
+//b1: xây dượng giao diện
+// b2: setup redux
+    //b2.1: tạo file reducer
+    //b2.2: xây dựng state ( kiểu dữ liệu và giá trị khởi tạo
+    //b2.3: xây dựng action + hàm reducer( kiểu dữ liệu + logic)
+    //b2.4: xây dựng store
+    //b2/5: bọc toàn bộ chương trình trong Provider
+//b3: Lấy state ra để sử dụng (useSelector)
+//b4: hiển thị dữ liệu theo state
